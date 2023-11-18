d24899ee950f8aa6b8e0fd35e66c8f7077b4c92b
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-18 12:26:44 +0600

    room data filtered by category

--- client/src/components/Rooms/Rooms.jsx
+++ client/src/components/Rooms/Rooms.jsx
@@ -1,14 +1,28 @@
 import { useEffect, useState } from 'react';
 import Card from './Card';
 import Container from '../Shared/Container';
+import { useSearchParams } from 'react-router-dom';
 
 const Rooms = () => {
   const [rooms, setRooms] = useState([]);
+  const [params, setParams] = useSearchParams();
+  const category = params?.get('category');
+
   useEffect(() => {
     fetch('/rooms.json')
       .then((res) => res.json())
-      .then((data) => setRooms(data));
-  }, []);
+      .then((data) => {
+        if (category) {
+          const filteredRoom = data?.filter(
+            (room) => room?.category === category
+          );
+          setRooms(filteredRoom);
+        } else {
+          setRooms(data);
+        }
+      });
+  }, [category]);
+
   return (
     <Container>
       <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
