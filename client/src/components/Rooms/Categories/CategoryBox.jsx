0558cbebcbe4850ead99bf6579c6fad7e62f6a20
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-18 12:09:18 +0600

    card and category added and also added conditionl style depend on params

---
+++ client/src/components/Rooms/Categories/CategoryBox.jsx
@@ -0,0 +1,36 @@
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import qs from 'query-string';
+
+// eslint-disable-next-line react/prop-types
+const CategoryBox = ({ label, icon: Icon, selected }) => {
+  const [params, setParams] = useSearchParams();
+  const navigate = useNavigate();
+  params.get('category');
+  const handleClick = () => {
+    let currentQuery = {};
+    if (params) {
+      currentQuery = qs.parse(params?.toString());
+
+      const updatedQuery = { ...currentQuery, category: label };
+
+      const url = qs.stringifyUrl({
+        url: '/',
+        query: updatedQuery,
+      });
+      navigate(url);
+    }
+  };
+  return (
+    <div
+      onClick={handleClick}
+      className={`flex flex-col items-center justify-center gap-2 p-3 border-b-4 hover:text-neutral-300 transition cursor-pointer ${
+        selected ? 'border-b-neutral-800 text-neutral-800' : ''
+      }`}
+    >
+      <Icon size={26}></Icon>
+      <div className="text-sm font-medium">{label}</div>
+    </div>
+  );
+};
+
+export default CategoryBox;
