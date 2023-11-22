03346077e8591959ddb733a444fa95b03d8042f4
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-22 13:59:06 +0600

    Profile Page created

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
@@ -12,12 +13,14 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HelmetProvider>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <Toaster />
-        <RouterProvider router={router} />
-      </AuthProvider>
-    </QueryClientProvider>
-  </HelmetProvider>
+  <React.StrictMode>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <Toaster />
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </QueryClientProvider>
+    </HelmetProvider>
+  </React.StrictMode>
 );
--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -13,6 +13,7 @@ import MyListing from '../pages/Dashboard/Host/MyListing';
 import HostRoute from './HostRoute';
 import AdminRoute from './AdminRoute';
 import ManageUsers from '../pages/Dashboard/Admin/ManageUsers';
+import Profile from '../pages/Dashboard/Common/Profile';
 
 export const router = createBrowserRouter([
   {
@@ -68,6 +69,14 @@ export const router = createBrowserRouter([
             <AdminRoute>
               <ManageUsers></ManageUsers>
             </AdminRoute>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: '/dashboard/profile',
+        element: (
+          <PrivateRoute>
+            <Profile></Profile>
           </PrivateRoute>
         ),
       },
